366ff923c9ac130e2e2e3e7134be5d8fd7290e32
Yifan <yifan.shen.17@gmail.com>
2020-10-13 17:04:48 +0100

    rebuild

--- utiles.py
+++ utiles.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 def cal_return(df):
     df_norm = pd.DataFrame()
@@ -16,58 +17,73 @@ def cal_return(df):
 
 
 def get_stats(weights, df_return):
+
     weights = np.array(weights)
     ret = np.sum(df_return.mean() * weights) * 252 
     PortVariance = np.dot(weights.T, np.dot(df_return.cov(), weights)) * 252 
     sharpe_ratio = ret/np.sqrt(PortVariance)
     vol = np.std(df_return) * np.sqrt(252)
     div = np.sum(vol * weights)/np.sqrt(PortVariance)
-    return [ret, np.sqrt(PortVariance), sharpe_ratio, div]
 
-def neg_sharpe(weights, df_return):
-    return get_stats(weights, df_return)[2] * (-1)
+    return {'return': ret,
+            'volatility': np.sqrt(PortVariance),
+            'sharpe_ratio': sharpe_ratio,
+            'diversification_ratio': div}
 
-def neg_diversification(weights, df_return):
-    return get_stats(weights, df_return)[3] * (-1)
+def negative_target(weights, df_return, target):
+    return get_stats(weights, df_return)[target] * (-1)
 
 def check_sum(weights):
     return np.sum(weights) - 1
 
 
-def portfolio_optimisation(df_return=df_return, 
-                           cutoff_date='2000-01-01', 
+def portfolio_optimisation(df_return,
+                           cutoff_date='2000-01-01',
                            upper_bound=1,
+                           lower_bound=0,
                            drop_strategy=['equity_momentum'],
-                           target='sharpe'):
+                           target='sharpe_ratio'):
     
-    df_return = df_return[df_return['Date']>cutoff_date]
+    df_return = df_return[df_return['Date']>=cutoff_date]
     
     try:
         df_return = df_return.drop(drop_strategy, axis=1)
-    except:
+    except ValueError:
         pass 
 
-    
     num_strategies = len(df_return.columns) - 1
     
     init_guess = np.array(np.random.random(num_strategies))
     init_guess /= np.sum(init_guess)
     
     cons = ({'type': 'eq', 'fun': check_sum})
-    bounds = tuple([(0,upper_bound) for i in range(num_strategies)])
-    opt_results = minimize(fun=neg_sharpe, 
+    bounds = tuple([(lower_bound, upper_bound) for i in range(num_strategies)])
+    opt_results = minimize(fun=negative_target,
                            x0=init_guess, 
-                           args=(df_return),
+                           args=(df_return, target),
                            method='SLSQP', 
                            bounds=bounds, 
                            constraints=cons)
 
-    ret, PortVariance, sharpe_ratio, div = get_stats(opt_results.x, df_return)
-    
+    stats_results = get_stats(opt_results.x, df_return)
+
+    ret = stats_results['return']
+    volatility = stats_results['volatility']
+    sharpe_ratio = stats_results['sharpe_ratio']
+    div = stats_results['diversification_ratio']
+
     df_portfolio = df_return[df_return.columns[1:]] * opt_results.x
     df_portfolio_sum = pd.DataFrame()
     df_portfolio_sum['Date'] = df_return['Date']
     df_portfolio_sum['perf'] = (1+df_portfolio.sum(axis=1)).cumprod()
     plt.plot(df_portfolio_sum['Date'], df_portfolio_sum['perf'])
-    
-    return -opt_results.fun, df_portfolio_sum, opt_results.x, ret, PortVariance, div
+
+    return {'objective': -opt_results.fun,
+            'df_portfolio_sum': df_portfolio_sum,
+            'weights': opt_results.x,
+            'return': ret,
+            'volatility': volatility,
+            'diversification_ratio': div,
+            'df_portfolio': df_portfolio,
+            'df_return': df_return,
+            'sharpe_ratio': sharpe_ratio}
