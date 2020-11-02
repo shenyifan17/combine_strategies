266c23f6163c91ee5d93ada7916435245c5604c7
Yifan <yifan.shen.17@gmail.com>
2020-11-02 09:07:19 +0000

    ädding file

---
+++ BloombergConnect.py
@@ -0,0 +1,30 @@
+"""""
+Tools for connecting bloomberg 
+"""
+
+class BloombergConnect(object):
+
+    def __init__(self):
+        """"
+        Initialise bloomberg connection
+        """
+        self.username = '123'
+        self.password = 'asd'
+        pass 
+
+
+    def download_by_index(self, 
+                          bbg_index, 
+                          start_date,
+                          end_date,
+                          save_local_copy=True):
+        
+        pass 
+
+    
+
+    def _end_(self):
+        """""
+        End connection
+        """
+        pass 
---
+++ Product.py
@@ -0,0 +1,23 @@
+"""""
+This class contains two portfoliio optimisation of Alternatives Investments 
+
+1. Alternative Risk Premium (ARP)
+2. Global Risk Mitigation (GRM)
+""""
+
+class AlternativeRiskPremium(object):
+
+    def __init__(self):
+
+        pass 
+
+
+class GlobalRiskMitigation(object):
+
+    """"
+    Non-linear optimisation? 
+    """
+
+    def __init__(self):
+
+        pass 
